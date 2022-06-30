aa6cc2f39bd6f19f8da31ccaf3665945c5a6eaaa
Agatha9626 <agatha.mathuva@strathmore.edu>
2022-06-30 14:07:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+#include <string.h>
+
+
+
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *  * main - Entry point
  *
- *   * Return: 0 if success.
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
 
 int main(void)
 
@@ -14,6 +22,9 @@ int main(void)
 
 	puts("\"Programming is like building a multilingual puzzle");
 
+
+
 	return (0);
 
 }
+
